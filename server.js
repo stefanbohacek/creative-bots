10e04ee540f0fcf2a6e3ede3610497877792c77c
System <none>
2017-03-14 04:06:01 +0000

    Exporting to fourtonfish/glitch-twitterbot-template

--- server.js
+++ server.js
@@ -24,13 +24,12 @@ T.post('statuses/update', { status: 'Hello world!' }, function(err, data, respon
 
 
 
-
 /**********************************************************************************************/
 /* The code below takes care of serving the index.html file, no need to change anything here. */
 
 var express = require('express');
 var app = express();
 app.use(express.static('public'));
-listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
