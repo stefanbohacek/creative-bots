88157e39cc44049f25791f35d0cfaf6fb8e92a89
System <none>
2017-03-27 03:39:54 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var path = require('path'),
 
 app.use(express.static('public'));
 
-/* You can use uptimerobot.com or a similar site to hit your /tweet endpoint to wake up your app and make your Twitter bot tweet. */
+/* You can use uptimerobot.com or a similar site to hit your /BOT_ENDPOINT to wake up your app and make your Twitter bot tweet. */
 
 app.all("/" + process.env.BOT_ENDPOINT, function (request, response) {
 /* The example below tweets out "Hello world!". */
