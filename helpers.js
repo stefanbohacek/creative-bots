d4a687a19106d1b66a011664b6de1a860153e1f8
Glitch (twitterbot) <none>
2018-03-27 19:19:57 +0000

    🏬🍝 Checkpoint ./twitter.js:178972/2724 ./helpers.js:178972/2421

---
+++ helpers.js
@@ -0,0 +1,69 @@
+var fs = require('fs'),
+    path = require('path'),
+    request = require('request');
+
+module.exports = {
+  random_from_array: function(arr) {
+    return arr[Math.floor(Math.random()*arr.length)]; 
+  },
+  get_random_int: function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  },
+  get_random_range: function(min, max, fixed) {
+    return (Math.random() * (max - min) + min).toFixed(fixed) * 1;
+  },
+  load_image_assets: function(cb){
+    /* Load images from the assets folder */
+    console.log('reading assets folder...')
+    var that = this;
+    fs.readFile('./.glitch-assets', 'utf8', function (err, data) {
+      if (err) {
+        console.log('error:', err);
+        return false;
+      }
+      data = data.split('\n');
+      var data_json = JSON.parse('[' + data.join(',').slice(0, -1) + ']'),
+          deleted_images = data_json.reduce(function(filtered, data_img) {
+              if (data_img.deleted) {
+                 var someNewValue = { name: data_img.name, newProperty: 'Foo' }
+                 filtered.push(data_img.uuid);
+              }
+              return filtered;
+            }, []),
+          img_urls = [];
+      
+      for (var i = 0, j = data.length; i < j; i++){
+        if (data[i].length){
+          var img_data = JSON.parse(data[i]),
+              image_url = img_data.url;
+
+          if (image_url && deleted_images.indexOf(img_data.uuid) === -1 && that.extension_check(image_url)){
+            var file_name = that.get_filename_from_url(image_url).split('%2F')[1];            
+            // console.log(`- ${file_name}`);
+            img_urls.push(image_url);
+          }
+        }
+      }
+      cb(null, img_urls);
+    });      
+  },
+  load_remote_image: function(img_url, cb) {
+    console.log(`loading remote image: ${img_url} ...`);
+    request({url: img_url, encoding: null}, function (err, res, body) {
+        if (!err && res.statusCode == 200) {
+          var b64content = 'data:' + res.headers['content-type'] + ';base64,';
+          cb(null, body.toString('base64'));           
+        } else {
+          console.log('ERROR:', err);
+          cb(err);
+        }
+    });
+  },  
+  extension_check: function(url) {
+    var extName = path.extname(url).toLowerCase();
+    return extName === ".png" || extName === ".jpg" || extName === ".jpeg";
+  },
+  get_filename_from_url: function(url) {
+    return url.substring(url.lastIndexOf('/') + 1);
+  },
+};
