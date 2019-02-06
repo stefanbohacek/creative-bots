d6a2405eb0d2220aec222fe0567fc3ca346425fe
Glitch (twitterbot-advanced) <none>
2019-02-06 13:56:43 +0000

    📊☄️ Checkpoint ./server.js:178972/65

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ app.use(express.static('public'));
 
 /* You can use uptimerobot.com or a similar site to hit your /BOT_ENDPOINT to wake up your app and make your Twitter bot tweet. */
 
-app.all("/" + process.env.BOT_ENDPOINT, function (req, res) {
+app.all(`/${process.env.BOT_ENDPOINT}`, function(req, res) {
 
   /* See EXAMPLES.js for some example code you can use. */
 
 });
 
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT, function(){
   console.log('your bot is running on port ' + listener.address().port);
 });
