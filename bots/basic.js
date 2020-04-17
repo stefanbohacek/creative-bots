b16a8bb24dc7961c35e25546ea8a05e7d06824ba
Glitch (twitterbot-advanced) <none>
2020-04-17 13:57:15 +0000

    ✊🏃 Checkpoint ./server.js:178972/153 ./bots/basic.js:178972/439 ./package.json:178972/933

--- bots/basic.js
+++ bots/basic.js
@@ -1,11 +1,17 @@
 let helpers = require(__dirname + '/../helpers/helpers.js'),
     twitter = require(__dirname + '/../helpers/twitter.js'),    
+    mastodon = require(__dirname + '/../helpers/mastodon.js'),    
     Twit = require( 'twit' ),
     T = new Twit( {
         consumer_key: process.env.TWITTER_CONSUMER_KEY_BOT_1,
         consumer_secret: process.env.TWITTER_CONSUMER_SECRET_BOT_1,
         access_token: process.env.TWITTER_ACCESS_TOKEN_BOT_1,
         access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET_BOT_1
+    } ),
+    Mastodon = require( 'mastodon' ),
+    M = new Mastodon( {
+     'access_token': process.env.MASTODON_ACCESS_TOKEN_BOT_1,
+     'api_url': process.env.MASTODON_API_BOT_1
     } );
 
 module.exports = {
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 /* Setting things up. */
+
 let path = require( 'path' ),
     express = require( 'express' ),
     app = express(),
@@ -6,24 +7,24 @@ let path = require( 'path' ),
 
 /* Load your bots */
 
-let bot1 = require( __dirname + '/bots/twitter-basic.js' ),
-    bot2 = require( __dirname + '/bots/twitter-random-image.js' );
+let bot1 = require( __dirname + '/bots/basic.js' ),
+    bot2 = require( __dirname + '/bots/random-image.js' );
 
 app.use( express.static( 'public' ) );
 
 let listener = app.listen( process.env.PORT, function(){
   console.log( `your bot is running on port ${ listener.address().port }` );
 
+  /* Schedule your bots */
+
   // Check out the cron package documentation for more details on how to set up cron jobs: https://www.npmjs.com/package/cron
 
-  let job1 = new CronJob( '0 * * * *', function() {
-    bot1.run();
-  } );
+//   ( new CronJob( '0 * * * *', function() {
+//     bot1.run();
+//   } ) ).start();
   
-  let job2 = new CronJob( '0 * * * *', function() {
-    bot2.run();
-  } );
+//   ( new CronJob( '0 * * * *', function() {
+//     bot2.run();
+//   } ) ).start();
   
-  job1.start();
-  job2.start();
 } );
